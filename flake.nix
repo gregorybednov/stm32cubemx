1f489a90c9013a31ac459d54bcd555edf867c1ab
Gregory Bednov <gregorbednov@gmail.com>
2025-01-03 02:27:22 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   };
 
-  outputs = { self, nixpkgs, lib, ... }:
+  outputs = { self, nixpkgs, ... }:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
 
