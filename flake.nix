353a5dda544ed0c9f7a59a93877b4b64bdfa5066
Gregory Bednov <gregorbednov@gmail.com>
2025-01-03 14:46:17 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
         pname = "stm32cubemx";
         version = "6.1.11";
         src = builtins.fetchTarball {
-          url = "http://kafpi.local/stm32cube_mx_v${
+          url = "http://kafpi.local/stm32cubemx_v${
           builtins.replaceStrings [ "." ] [ "" ] version
           }-lin.tar.gz";
           sha256 = "";
